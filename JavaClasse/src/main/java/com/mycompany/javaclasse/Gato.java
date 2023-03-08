5792550379aaa202beff8deae4c2efc5144eb88f
Fernanda Hallmann <111521249+FernandaHallmann@users.noreply.github.com>
2023-03-08 19:17:32 -0300

    Update Gato.java

--- JavaClasse/src/main/java/com/mycompany/javaclasse/Gato.java
+++ JavaClasse/src/main/java/com/mycompany/javaclasse/Gato.java
@@ -4,6 +4,9 @@ package com.mycompany.javaclasse;
  *
  * @author Ferna
  */
+
+//Fazer uma classe distinguindo com 5 atributos para definir um animal depois criar outra classe para cadastrar 5 animais
+
 public class Gato {
     
     char sexo;
